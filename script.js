325fdcde4bc3fd8350e5aca97acf70fe9439859f
Vijendra-21 <vijendrakumawat8710@gmail.com>
2022-05-26 15:06:42 +0530

    fixing projects section

--- script.js
+++ script.js
@@ -42,7 +42,7 @@
         for (var i = 0; i < btns.length; i++) {
           btns[i].addEventListener("click", function() {
             var current = document.getElementsByClassName("active");
-            current[0].className = current[0].className.replace(" active", "");
+            current[1].className = current[1].className.replace(" active", "");
             this.className += " active";
           });
         }        
